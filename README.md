68d48cd45d4fef25c1e1f7595420706393461e0d
EndrXOriginal <chris.pin@hotmail.ca>
2023-03-13 13:31:50 -0400

    Add text to README.md file and jokes to script.js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-#Message Generator
+# Message Generator
+## What it's about:
+
+This code is a simple javaScript code that I used to familiarize myself with gitbash and github. I also used it to test out the usage of markdown for this read me.
+
+## What it does:
+
+Upon running the code, it will print a random joke to the console.
+There is 5 jokes in total, enjoy running it over and over to see them all!
--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('Hello World');
+let jokeArray = ["I'm afraid for the calendar. Its days are numbered.", "Why do fathers take an extra pair of socks when they go golfing? In case they get a hole in one!", "What do you call a factory that makes okay products? A satisfactory.",
+"I asked my dog what's two minus two. He said nothing.", "I don't trust those trees. They seem kind of shady."];
+
+console.log(jokeArray.length)
