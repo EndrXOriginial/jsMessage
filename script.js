b9fbdf2eb828e7cc5c89cde0385ed9be84044716
EndrXOriginal <chris.pin@hotmail.ca>
2023-03-13 13:58:06 -0400

    Finished code upon activation

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let jokeArray = ["I'm afraid for the calendar. Its days are numbered.", "Why do fathers take an extra pair of socks when they go golfing? In case they get a hole in one!", "What do you call a factory that makes okay products? A satisfactory.",
 "I asked my dog what's two minus two. He said nothing.", "I don't trust those trees. They seem kind of shady."];
 
-console.log(jokeArray.length)
+// Creates a random number between 0 and the lenght of jokeArray
+const randIndex = () => {
+    return Math.ceil(Math.random() * (jokeArray.length - 1));
+}
+
+// Function takes the array of jokes and prints the one of the jokes using the randIndex on the passed in array
+const postMessage = arr => {
+    let indexNum = randIndex();
+    console.log(indexNum);
+    return arr[indexNum];
+}
+
+console.log(postMessage(jokeArray));
